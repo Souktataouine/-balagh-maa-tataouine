f02f1c35969c4d77734d53cf88b77a183610d423
Souktataouine <rogbatv24@gmail.com>
2026-08-04 00:18:09 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,1 +1,19 @@
+const PASSWORD = "Tataouine2026";
 
+window.login = function () {
+
+    const password = document.getElementById("password").value;
+
+    if (password === PASSWORD) {
+
+        localStorage.setItem("admin", "true");
+
+        window.location.href = "admin.html";
+
+    } else {
+
+        alert("❌ كلمة المرور غير صحيحة");
+
+    }
+
+}
