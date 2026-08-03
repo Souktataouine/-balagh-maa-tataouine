222f444ed81a78ce7e1abb249fc55e5e5cbbdcf6
Souktataouine <rogbatv24@gmail.com>
2026-08-04 01:03:57 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,25 +1,27 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 import {
-    getFirestore
+  getFirestore
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyBJ1ayE2j6QcnhNkS-yJ8W3H7B3UpH5UHA",
+  apiKey: "AIzaSyBJ1ayE2j6QcnhNkS-yJ8W3H7B3UpH5UHA",
 
-    authDomain: "balagh-maa-tataouine.firebaseapp.com",
+  authDomain: "balagh-maa-tataouine.firebaseapp.com",
 
-    projectId: "balagh-maa-tataouine",
+  projectId: "balagh-maa-tataouine",
 
-    storageBucket: "balagh-maa-tataouine.firebasestorage.app",
+  storageBucket: "balagh-maa-tataouine.firebasestorage.app",
 
-    messagingSenderId: "511775432495",
+  messagingSenderId: "511775432495",
 
-    appId: "1:511775432495:web:a8c440db80eba59dd40bda"
+  appId: "1:511775432495:web:a8c440db80eba59dd40bda"
 
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
